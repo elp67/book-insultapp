385a5290a470c31412227e5934e5a8c919b1d8d4
Eddie Parker <linuxdude929@gmail.com>
2016-08-14 15:59:37 -0500

    fix syntax errors

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -16,9 +16,26 @@ public class InsultGenerator {
       databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
       databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
       String username = System.getenv("POSTGRESQL_USER");
-      String password = System.getent("PGPASSWORD");
+      String password = System.getenv("PGPASSWORD");
       Connection connection = DriverManager.getConnection(databaseURL, username, password);
 
       if (connection != null) {
-        String SQL = "select a.string AS first, b.string AS second, c.string AS noun
-          from short_adjective a, long adjective b, noun c ORDER BY random() limit 1";
+        String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a, long adjective b, noun c ORDER BY random() limit 1";
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(SQL);
+        while(rs.next()) {
+          if (vowels.indexOf(rs.getString("first")) == -1) {
+            article = "a";
+          }
+          theInsult = String.format("Thou art %s %s %s %s!",
+              article, rs.getString("first"), rs.getString("second"), rs.getString("noun"));
+        }
+        rs.close();
+        connection.close();
+      }
+    } catch (Exception e) {
+      return "Database connection problem!";
+    }
+    return theInsult;
+  }
+}
